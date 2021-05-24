8412dc59edb1145c49d88089f7d842d5993e8a39
chika.ootsuku <c-otsuku@rosca.jp>
2021-05-24 06:35:29 +0000

    kadai-ruby-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,27 +1,15 @@
-# FizzBuzzの判定をするメソッド
- def fizzbuzz(num)
-　 return:FizzBuzz if num % 15 == 0
-　 return:Buzz if num % 5 == 0
-　 return:Fizz if num % 3 == 0
- 　num
- end
 num_max = 100
 def fizzbuzz(num)
   if num % 15 == 0
     return "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   else
-    puts num
+    return num
   end
 end 
  (1..num_max).each do |num|
-   puts fizzbuzz(num)
+  puts fizzbuzz(num)
 end
-# 以下は動作確認用の記述です
-puts fizzbuzz(1)
-puts fizzbuzz(3)
-puts fizzbuzz(5)
-puts fizzbuzz(15)
